149b0615a7e7474b51724c819e939b395f31c4ea
NomiaPaimoN <112900074+NomiaPaimoN@users.noreply.github.com>
2023-03-20 01:05:51 +0300

    Update я геймер 228.cpp

--- я геймер 228.cpp
+++ я геймер 228.cpp
@@ -1,61 +1,48 @@
-﻿#include <iostream>
-#include <math.h>
-#include <conio.h>
-#include <iomanip>
-
+#include <iostream>
 using namespace std;
-typedef double (*uf)(double, double, int&);
-void tabl(double, double, double, double, uf, uf);
-double y(double, double, int&);
-double s(double, double, int&);
 
-
-int main()
-{
-    setlocale(LC_ALL, "ru");
-    double a = 0.1, b = 1, h = 0.1, ep = 0;
-    cout << "Нажмите 1 для автоматического ввода\n";
-
-    if (_getch() == '1') {
-        cout << "Введите е: "; cin >> ep;
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
     }
-    else {
-        cout << "Введите a, b, h, e" << endl;
-        cin >> a >> b >> h >> ep;
+    for (int i = 2; i <= n/2; i++) {
+        if (n % i == 0) {
+            return false;
+        }
     }
+    return true;
+}
 
-    cout << setw(10) << "X" << setw(15) << "Y(x)" << setw(15) << "S(x)" << setw(8) << "k" << endl;
-    tabl(a, b, h, ep, y, s);
-    cout << endl;
+int countPrimesRecursive(int x, int i) {
+    if (i > x) {
+        return 0;
+    }
+    if (isPrime(i)) {
+        return 1 + countPrimesRecursive(x, i+1);
+    }
+    return countPrimesRecursive(x, i+1);
+}
+
+int countPrimes(int x) {
+    int count = 0;
+    for (int i = 2; i <= x; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int x;
+    cout << "Введите число x: ";
+    cin >> x;
+
+    int count1 = countPrimes(x);
+    int count2 = countPrimesRecursive(x, 2);
+
+    cout << "Количество простых чисел не превосходящих " << x << " (нерекурсивный): " << count1 << endl;
+    cout << "Количество простых чисел не превосходящих " << x << " (рекурсивный): " << count2 << endl;
+
     return 0;
-}
-
-void tabl(double a, double b, double h, double eps, uf fun, uf fun2) //OutRez
-{
-    int k = 0;
-    double sum = 0, sum2 = 0;
-    for (double x = a; x < b + h / 2; x += h)
-    {
-        sum = fun(x, eps, k);
-        sum2 = fun2(x, eps, k);
-        cout << setw(10) << x << setw(15) << sum << setw(15) << sum2 << setw(8) << k << endl;
-    }
-}
-
-double y(double x, double eps, int& k)
-{
-    return exp(2 * x);
-}
-
-double s(double x, double eps, int& k)
-{
-    double p = 1, sum = 1;
-    k = 1;
-    while (fabs(p) > eps)
-    {
-        p *= 2 * x / k;
-        sum += p;
-        k++;
-    }
-    return sum;
 }
